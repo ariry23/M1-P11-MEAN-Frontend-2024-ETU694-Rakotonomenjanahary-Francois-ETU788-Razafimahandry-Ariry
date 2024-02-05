0b9be9193de97d2adbdeb664d64191f08f4c71a3
ariry23 <52358805+ariry23@users.noreply.github.com>
2024-02-05 22:53:10 +0300

    page de reservation

--- src/app/demo/service/list/list.component.ts
+++ src/app/demo/service/list/list.component.ts
@@ -1,23 +1,33 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 
 @Component({
-  selector: 'app-list',
+  selector: 'service-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
   standalone : true , 
   imports: [CommonModule, SharedModule  , NgOptimizedImage],
 })
 export default class ListComponent {
+  constructor(private router : Router){}
   isMouseOver: boolean = false;
   onMouseOver(): void {
     this.isMouseOver = true;
+    
   }
 
   onMouseLeave(): void {
     this.isMouseOver = false;
+  }
+
+  getDetail(id)
+  {
+    console.log("get details") ; 
+      this.router.navigate(['/service/detail', id]);
+      
   }
 }
