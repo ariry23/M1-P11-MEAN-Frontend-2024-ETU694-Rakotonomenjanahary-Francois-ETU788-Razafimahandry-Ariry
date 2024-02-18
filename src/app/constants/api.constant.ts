55d45c7cc164d026e75a5ed4dca582171e764be3
Francois Rakoto <50391876+HDRHawking@users.noreply.github.com>
2024-02-18 15:04:40 +0300

    liste reservation par employe

--- src/app/constants/api.constant.ts
+++ src/app/constants/api.constant.ts
@@ -2,3 +2,4 @@ export const PERSONNEL_LIST = 'personnel/list' ;
 export const PERSONNEL_UPDATE = 'personnel/update' ; 
 export const PERSONNEL_AJOUT = 'personnel/ajout' ; 
 export const PERSONNEL_DELETE = 'personnel/delete' ; 
+export const RENDEZ_VOUS_LIST = 'user/list-employee';
--- src/app/demo/pages/rendez-vous/list/list.component.ts
+++ src/app/demo/pages/rendez-vous/list/list.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ListBasePageComponent } from '../../../ui-elements/page/list-base-page/list-base-page.component';
+import { ApiService } from 'src/app/core/services/api.service';
+
+import { ToastrService } from 'ngx-toastr';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { RENDEZ_VOUS_LIST } from 'src/app/constants/api.constant';
+
+import { CommonModule } from '@angular/common';
+import { TokenService } from 'src/app/core/services/token.service';
+import { jwtDecode } from 'jwt-decode';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  standalone: true,
+  imports: [ CommonModule] 
 })
-export default class ListComponent {
-  
+export default class ListComponent implements OnInit{
+
+  data: any;
+
+  constructor(private apiService: ApiService, private toastrService: ToastrService, private tokenService: TokenService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.getData(); 
+  }
+
+  getData(): void {
+    let userConnected : any = jwtDecode(this.tokenService.getToken());
+    let apiData = {
+      idempl: userConnected.user._id,
+    }
+    this.apiService.postData(RENDEZ_VOUS_LIST,apiData).subscribe(datas => {
+      this.data = datas.data;
+      console.log(this.data);
+    }, err => {
+      this.toastrService.error(err);
+    })
+  }
 }
