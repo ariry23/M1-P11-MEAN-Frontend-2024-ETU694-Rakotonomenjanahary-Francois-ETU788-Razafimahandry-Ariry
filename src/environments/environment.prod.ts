b7a12fd80f6a9b209a7e172a0fe0543a3528f5d6
ariry23 <52358805+ariry23@users.noreply.github.com>
2024-02-29 06:36:35 +0300

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiBaseUrl: 'https://back-end-express-ws.onrender.com',
+  apiBaseUrl: 'http://localhost:3000',
 };
